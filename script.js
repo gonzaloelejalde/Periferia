67f43adf041259c241572895e926e3b4ec4b2d7e
Asdrúbal de Elejalde <asdrubalolocco@gmail.com>
2022-07-21 23:55:00 -0300

    correciones

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-console.log(document.getElementById("parrafo1"))
-const parrafo1 = document.getElementById("parrafo1")
-
-let productoABuscar = prompt("Ingrese un producto de nuestro stock, tal cual este escrito, para consultar por su marca, precio y stock: Auricular, Monitor, Mouse, Teclado y/o Mousepad").toUpperCase()
-let nombreUser = prompt("Ingrese su nombre")
-let apellidoUser = prompt("Ingrese su apellido")
-let valorPrestamo = 0 
-let pregunta, saludos, respuesta, continua
-
-do{
-    horario = parseInt(prompt("Ingrese su hora actual"))
-    if(horario <= 12 ){
-        saludos = "buenos dias"
-    }else if(horario <= 21 ){
-        saludos = "buenas tardes"
-    }else if(horario <= 24 ){
-        saludos = "buenas noches"
-    }else if(horario <= 5){
-        saludos = "¿Qué haces despierto a esta hora?"
-    }else if (horario >= 25){
-        alert("Esos horarios no existen")
-    }else if(isNaN (horario)){
-        alert("Esos horarios no existen")
-    }
-}while(isNaN(horario) || horario >= 25) 
-
-const user = {username: nombreUser, lastname: apellidoUser, time: horario}
-parrafo1.innerText = `Hola ${user.username} ${user.lastname}, ${saludos}`
-
 class Producto {
     constructor (id, nombre, marca, precio, stock, imagen){
     this.id = id
@@ -49,148 +20,110 @@ const producto5 = new Producto (5, "MOUSEPAD", "Hyperx", 2000, 19, `img/mousepad
 
 const productos = [producto1, producto2, producto3, producto4, producto5]
 
-function buscarProducto(productos) {
-    let nombreProducto = prompt("Ingrese un nombre de periferico (Aparecera en la consola)").toUpperCase()
-    console.log(productos.find(productos => productos.nombre == nombreProducto))
-    if(nombreProducto !== "AURICULAR" && nombreProducto !== "MONITOR" && nombreProducto !== "MOUSE" && nombreProducto !== "TECLADO" && nombreProducto !== "MOUSEPAD"){
-        console.log("Producto no encontrado")
-    }
-}
-
-function buscarProductos(productos) {
-    let precio = parseFloat(prompt("Ingrese un precio"))
-    let buscarProducto = productos.filter(producto => producto.precio >= precio)
-    if (buscarProducto.length == 0) {
-        console.log("No hay productos con dichas caracteristicas")
-    } else {
-        console.log(buscarProducto)
-    }
-}
-
-function agregarProductos(productos){
-    let agregarProducto = prompt("Agregue un producto para que nosotros podamos saber que es lo que necesita el cliente").toUpperCase()
-    if (agregarProducto !== "AURICULAR" && agregarProducto !== "MONITOR" && agregarProducto !== "MOUSE" && agregarProducto !== "TECLADO" && agregarProducto !== "MOUSEPAD"){
-        console.log(`Se agrego correctamente ${agregarProducto}`)
-        alert(`Se agrego correctamente el siguiente producto: ${agregarProducto}`)
-        console.log(productos.push(agregarProducto))
-        console.log(productos)
-        console.log(new Producto (6, `${agregarProducto}`, "Predetermiado", "-", "0", "-"))
-    }else{
-        alert("Este producto ya existe")
-    }
-}
-
-
 const divProductos = document.getElementById("productos")
 
-for(let producto of productos){
-    divProductos.innerHTML += `
-    <div class="card productos" id="producto${producto.id}" style="width: 18rem;">
-    <div class="card-body">
-        <h5 class="card-title">${producto.nombre}</h5>
-        <p class="card-text">Marca: ${producto.marca}</p>
-        <p class="card-text">Precio: $${producto.precio}</p>
-        <p class="card-text">Stock: ${producto.stock}</p>
-        <div class = "imagen"><img src = "${producto.imagen}"></div>
+function renderizarProductos(productos){
+    for(let producto of productos)
+{
+        divProductos.innerHTML += `
+        <div class="card productos" id="producto${producto.id}" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">${producto.nombre}</h5>
+            <p class="card-text">Marca: ${producto.marca}</p>
+            <p class="card-text">Precio: $${producto.precio}</p>
+            <p class="card-text">Stock: ${producto.stock}</p>
+            <div class = "imagen"><img src = "${producto.imagen}"></div>
+            </div>
         </div>
-    </div>
-    </div>
-    `
-}
-
-while(productoABuscar !== "AURICULAR" && productoABuscar !== "MONITOR" && productoABuscar !== "MOUSE" && productoABuscar !== "TECLADO" && productoABuscar !== "MOUSEPAD"){
-    productoABuscar = prompt("Ingrese un producto de nuestro stock, tal cual este escrito, para consultar por su marca, precio y stock: Auricular, Monitor, Mouse, Teclado y/o Mousepad").toUpperCase()
-}
-
-if(productoABuscar == "AURICULAR"){
-    (producto1.mostrarMensaje())
-}else if(productoABuscar == "MONITOR"){
-    (producto2.mostrarMensaje())
-}else if(productoABuscar == "MOUSE"){
-    (producto3.mostrarMensaje())
-}else if(productoABuscar == "TECLADO"){
-    (producto4.mostrarMensaje())
-}else if(productoABuscar == "MOUSEPAD"){
-    (producto5.mostrarMensaje())
-}
-
-do {
-    do{
-        continua = prompt("¿Desea realizar otra función?").toUpperCase()
-        if (continua == "SI"){
-            do{
-                respuesta = parseInt(prompt(`Ingrese un número para: 
-                1- Buscar un producto
-                2- Buscar productos por precio
-                3- Agregar producto
-                `))} while (respuesta < 1 || respuesta > 3)
-        }
-        else if(continua != "SI"){
-            alert("Usted a ingresado No")
-            break
-        }
-    } while(continua != "SI")
-} while((continua != "SI") && (respuesta < 1 || respuesta > 3))
-
-switch(respuesta){
-    case 1:
-        buscarProducto(productos)
-        break
-    case 2:
-        buscarProductos(productos)
-        break
-    case 3:
-        agregarProductos(productos)
-        break
-    default:
-        alert("Continuar")
-        break
-}
-
-do {
-    pregunta = prompt("Quiere sacar un prestamo para poder comprar nuestros productos. Si o No (Unicamente valido en Periferia)").toUpperCase()
-    do{
-    if(pregunta == "SI"){
-        valorPrestamo = parseInt (prompt("Ingrese un prestamo"))
-    if(isNaN(valorPrestamo)){
-        alert("Por favor ingrese numeros válidos")
-    }
-    if(valorPrestamo === 0){
-        alert("No se puede pedir un préstamo por 0")
-    }
-    }else if(pregunta == "NO")
-        alert("Hasta luego!")
-        break
-    }while((isNaN(valorPrestamo)) || (valorPrestamo === 0))
-    if(pregunta === "NO"){
-        break
-    }
-}while(pregunta != "SI")
-
-const IVA = 1.21
-
-class Prestamo {
-    constructor (valorPrestamo, IVA) {
-        this.valorPrestamo = valorPrestamo;
-        this.IVA = IVA;
-        this.precioFinal = this.valorPrestamo * IVA;
-    }
-    aplicarCuotas(){
-        this.cuotas = this.precioFinal / 3
-        this.precioAumento = (this.precioFinal * 1.05) / 6
-    }
-    mostrarMensaje(){
-        alert(`Cuando devuelvas el préstamo, vas a tener que pagar ${this.precioFinal} pesos argentinos, ya que aumenta por la inflación. Lo podes abonar en 3 cuotas de ${this.cuotas} sin interés o en 6 cuotas de ${this.precioAumento} con un interés del 5%, ambas con el aumento de los impuestos sobre el valor añadido`)
+        </div>
+        `
     }
 }
 
-const prestamo1 = new Prestamo(valorPrestamo,IVA)
+renderizarProductos(productos)
 
-prestamo1.aplicarCuotas()
 
-if(pregunta ==  "SI"){
-prestamo1.mostrarMensaje()
+
+let carrito = []
+
+if(localStorage.getItem("carrito")){
+    carrito = JSON.parse (localStorage.getItem("carrito"))
+}else{
+    localStorage.setItem("carrito", JSON.stringify(carrito))
 }
+
+const botonCarrito = document.getElementById("botonCarrito")
+const divCarrito = document.getElementById("divCarrito")
+const botonAñadir = document.getElementById("botonAñadir")
+const idFormCarrito = document.getElementById("idFormCarrito")
+const idProducto = document.getElementById("idProducto")
+
+
+
+idFormCarrito.addEventListener('submit', (event) => {
+    event.preventDefault() 
+    console.log(event.target)
+
+    let datForm = new FormData(event.target) 
+
+    console.log(datForm.get("nombreProducto"))
+
+    const carritos = new Producto(productos)
+
+    carrito.push(carritos)
+    console.log(carrito)
+    
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+
+    form.reset()
+})
+
+botonCarrito.addEventListener("click", () => {
+    let carritoStorage = JSON.parse(localStorage.getItem("carrito"))
+    divCarrito.innerHTML = ""
+    
+    carritoStorage.forEach((producto, indice) =>{
+        divCarrito.innerHTML += `
+        <div class="card border-dark mb-3" id ="producto ${indice}" style="max-width: 20rem; margin: 4px;">
+            <div class="card-header"><h2></h2>${producto.nombre}</h2></div>
+            <div class="card-body"> 
+                <p class="card-title">${producto.precio}</p>
+                <button class = "btn btn-danger">Eliminar Producto</button>
+            </div>
+        </div>`
+    })
+
+    carritoStorage.forEach((producto, indice) => {
+        let carritoBoton = document.getElementById(`producto ${indice}`).lastElementChild.lastElementChild
+
+        carritoBoton.addEventListener("click", () => {
+            document.getElementById(`producto ${indice}`).remove()
+            carrito.splice(indice, 1)
+            localStorage.setItem("carrito", JSON.stringify(carrito))
+            console.log(`${producto.nombre} Eliminada`)
+        })
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Registro de Usuarios
 
 class Usuario {
     constructor(username, email, password){
@@ -200,41 +133,88 @@ class Usuario {
     }
 }
 
-const usuarios = []
+let usuarios = []
+
+if(localStorage.getItem("usuarios")){
+    usuarios = JSON.parse (localStorage.getItem("usuarios"))
+}else{
+    localStorage.setItem("usuarios", JSON.stringify(usuarios))
+}
 
 const form = document.getElementById("idForm")
 const divUsers = document.getElementById("divUsers")
 const botonUsers = document.getElementById("botonUsers")
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault()
+form.addEventListener('submit', (event) => {
+    event.preventDefault() 
+    console.log(event.target)
 
-    let username = document.getElementById("idUser").value
-    let email = document.getElementById("idEmail").value
-    let password = document.getElementById("idPassword").value
+    let datForm = new FormData(event.target) 
 
-    const usuario = new Usuario (username, email, password)
+    console.log(datForm.get("user"), datForm.get("email"), datForm.get("password"))
+
+    const usuario = new Usuario (datForm.get("user"), datForm.get("email"), datForm.get("password"))
+
     usuarios.push(usuario)
     console.log(usuarios)
+    
+    localStorage.setItem("usuarios", JSON.stringify(usuarios))
 
-    form.reset() 
+    form.reset()
 })
 
 botonUsers.addEventListener("click", () => {
-    usuarios.forEach(usuario => {
+    let arrayStorage = JSON.parse(localStorage.getItem("usuarios"))
+    divUsers.innerHTML = ""
+    
+    arrayStorage.forEach((usuario, indice) =>{
         divUsers.innerHTML += `
-        <div class = "card" style = "width: 18rem; margin:3px;">
-            <div class = "card-body>
-                <h5 class = "card-title">${usuario.username}</h5>
-                <p class = "card-text">${usuario.email}</p>
+        <div class="card border-dark mb-3" id ="usuario ${indice}" style="max-width: 20rem; margin: 4px;">
+            <div class="card-header"><h2></h2>${usuario.username}</h2></div>
+            <div class="card-body"> 
+                <p class="card-title">${usuario.email}</p>
+                <button class = "btn btn-danger">Eliminar Tarea</button>
             </div>
         </div>`
     })
+
+    arrayStorage.forEach((usuario, indice) => {
+        let botonCard = document.getElementById(`usuario ${indice}`).lastElementChild.lastElementChild
+
+        botonCard.addEventListener("click", () => {
+            document.getElementById(`usuario ${indice}`).remove()
+            usuarios.splice(indice, 1)
+            localStorage.setItem("usuarios", JSON.stringify(usuarios))
+            console.log(`${usuario.username} Eliminada`)
+        })
+    })
 })
 
-const inputColor = document.getElementById("inputColor")
+//Dropdown de Tema (Blanco o Negro)
 
-inputColor.addEventListener("input", () => {
-    document.body.style.backgroundColor = inputColor.value
-    console.log(inputColor.value)
+const botonDarkMode = document.getElementById("botonDarkMode")
+const botonLightMode = document.getElementById("botonLightMode")
+
+let darkMode
+
+if(localStorage.getItem('theme')){          
+    darkMode = localStorage.getItem('theme') 
+}else {
+    localStorage.setItem('theme', "light")  
+} 
+
+if(darkMode == "dark"){ 
+    document.body.classList.add('darkMode')
+}
+
+botonDarkMode.addEventListener("click", () => {
+
+    document.body.classList.add('darkMode')
+    localStorage.setItem('theme', "dark")
+})
+
+botonLightMode.addEventListener("click", () => {
+
+    document.body.classList.remove('darkMode')
+    localStorage.setItem('theme', "light")
 })
